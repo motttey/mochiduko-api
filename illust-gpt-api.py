50c60672382eaaf54bb0eb31772aaf4175e58e44
motttey <motitago@gmail.com>
2023-07-16 20:28:49 +0900

    fix: fetch image from openai

--- illust-gpt-api.py
+++ illust-gpt-api.py
@@ -2,10 +2,36 @@
 import json
 import codecs
 import os
+import requests
+import urllib.request
+
+def generate_image_response(prompt=''):
+	API_KEY = os.environ.get("CHATGPT_TOKEN")
+
+	# promptがない場合
+	if not prompt:
+		return
+
+	headers = {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer ' + API_KEY
+	}
+
+	data = {
+		# 'model': 'text-curie-001', # 動作テスト用（料金的に）
+		'prompt': prompt,
+		"n": 2,
+		"size": "1024x1024"
+	}
+
+	response = requests.post('https://api.openai.com/v1/images/generations', headers=headers, data=json.dumps(data))
+
+	# レスポンス受け取り後の処理
+	response_data = response.json()
+	return response_data
 
 if __name__ == '__main__':
-    ghatgpt_api_token = os.environ.get("CHATGPT_API_TOKEN")
-
+    '''
     sample_data = {
         "created": 1688992473,
         "data": [
@@ -17,6 +43,21 @@ if __name__ == '__main__':
             }
         ]
     }
+    '''
+
+    response_data = generate_image_response(prompt='Doraemon in Japanese Cartoon Style')
+    illust_data = response_data["data"]
+    output_array = []
+    for index, illust in enumerate(illust_data):
+        fname = "generated_1_{0}.jpg"
+        urllib.request.urlretrieve(illust["url"], fname.format(index))
+        output_array.append({
+              "image": fname,
+              "url": illust["url"]
+        })
 
     f1 = codecs.open("public/generated_doraemon_illust.json", "w", "utf-8")
-    json.dump(sample_data, f1, ensure_ascii=False)
+    json.dump({
+          "created": response_data,
+          "data": output_array
+    }, f1, ensure_ascii=False)
